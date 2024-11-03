05c9d648c1ab406d63d93476ab1dfd9b6e98e9bd
Hossein <mohamadhjf@gmail.com>
2024-11-04 02:25:44 +0330

    Completing the infrastructure

---
+++ HW11/Container/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11.Container
+{
+    public interface IProductRepository
+    {
+        void Add();
+        void Get(int id);
+        void GetAll();
+        void Update(int id);
+        void Delete(int id);
+
+    }
+}
---
+++ HW11/Repository/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using HW11.Container;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
