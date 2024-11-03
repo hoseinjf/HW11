032e77a669ae36a72b5844226385a0beef6b5106
Hossein <mohamadhjf@gmail.com>
2024-11-04 02:31:07 +0330

    entity check null

--- HW11/Entity/Category.cs
+++ HW11/Entity/Category.cs
@@ -9,6 +9,6 @@ namespace HW11.Entity
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
--- HW11/Entity/Product.cs
+++ HW11/Entity/Product.cs
@@ -9,9 +9,9 @@ namespace HW11.Entity
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int CategoryId { get; set; }
-        public int Price { get; set; }
+        public string? Name { get; set; }
+        public int? CategoryId { get; set; }
+        public int? Price { get; set; }
 
     }
 }
